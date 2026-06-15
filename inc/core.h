999ccf4c44004adb18dee02eff99a84345a70f83
pnaessen <pnaessen@student.42lyon.fr>
2026-06-15 07:34:46 -0400

    Update .h to receive_hop_packets

--- inc/core.h
+++ inc/core.h
@@ -8,7 +8,7 @@ int resolve_dns(const char *host, struct sockaddr_in *dest);
 int create_socket(t_traceroute *tr);
 void run_traceroute(t_traceroute *tr);
 void send_icmp_packet(t_traceroute *tr, int seq);
-void receive_packet(t_traceroute *tr, t_probe_result *res, int expected_seq, double start_time);
+void receive_hop_packets(t_traceroute *tr, t_probe_result *results, int *seqs, double *start_times, int num_probes);
 void send_udp_packet(t_traceroute *tr, int seq);
 
 #endif
