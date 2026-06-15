48d2bb20fb4ae4b27ceef663ba5842aa8bb81b60
pnaessen <pnaessen@student.42lyon.fr>
2026-06-15 07:34:05 -0400

    Update probes.c to support parallel probes

--- src/probes.c
+++ src/probes.c
@@ -1,29 +1,5 @@
 #include "ft_traceroute.h"
 
-static t_probe_result perform_one_probe(t_traceroute *tr, int seq)
-{
-    t_probe_result result;
-    double start_time;
-
-    memset(&result, 0, sizeof(result));
-
-    start_time = get_time_now();
-
-    if (tr->use_icmp)
-	send_icmp_packet(tr, seq);
-    else
-	send_udp_packet(tr, seq);
-
-    receive_packet(tr, &result, seq, start_time); //need to move recv after send all packet not only one 
-    if (result.got_reply) {
-	result.rtt = (get_time_now() - start_time) * 1000.0;
-	if (result.type == ICMP_ECHOREPLY || result.type == ICMP_DEST_UNREACH)
-	    result.is_final = true;
-    }
-
-    return result;
-}
-
 static void print_probe_result(t_probe_result *res, char *last_ip, bool resolve_dns)
 {
     if (!res->got_reply) {
@@ -56,15 +32,28 @@ static bool run_hop(t_traceroute *tr, int ttl)
 
     printf("%2d ", ttl);
 
+    t_probe_result results[tr->probes_per_hop];
+    double start_times[tr->probes_per_hop];
+    int seqs[tr->probes_per_hop];
+
+    memset(results, 0, sizeof(results));
+
     for (int i = 0; i < tr->probes_per_hop; i++) {
-	t_probe_result res = perform_one_probe(tr, global_seq++);
+	seqs[i] = global_seq++;
+	start_times[i] = get_time_now();
+	if (tr->use_icmp)
+	    send_icmp_packet(tr, seqs[i]);
+	else
+	    send_udp_packet(tr, seqs[i]);
+    }
 
-	print_probe_result(&res, last_ip_on_line, tr->resolve_dns);
+    receive_hop_packets(tr, results, seqs, start_times, tr->probes_per_hop);
 
-	if (res.is_final)
+    for (int i = 0; i < tr->probes_per_hop; i++) {
+	print_probe_result(&results[i], last_ip_on_line, tr->resolve_dns);
+	if (results[i].is_final)
 	    reached_dest = 1;
-
-	// fflush(stdout);
+	//fflush(stdout);
     }
 
     printf("\n");
